37bd593deaa389286ad24eb8a38e1b8899d6e499
HJacobH <119511593+HJacobH@users.noreply.github.com>
2024-10-19 14:53:29 +0200

    combobox

--- HospodaUBobra/Form1.Designer.cs
+++ HospodaUBobra/Form1.Designer.cs
@@ -29,22 +29,32 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            comboBoxTables = new ComboBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(21, 12);
+            dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(767, 426);
+            dataGridView1.Size = new Size(817, 426);
             dataGridView1.TabIndex = 0;
+            // 
+            // comboBoxTables
+            // 
+            comboBoxTables.FormattingEnabled = true;
+            comboBoxTables.Location = new Point(835, 12);
+            comboBoxTables.Name = "comboBoxTables";
+            comboBoxTables.Size = new Size(121, 23);
+            comboBoxTables.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(968, 450);
+            Controls.Add(comboBoxTables);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
@@ -55,5 +65,6 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private ComboBox comboBoxTables;
     }
 }
--- HospodaUBobra/Form1.cs
+++ HospodaUBobra/Form1.cs
@@ -3,55 +3,86 @@ using System.Data;
 using System;
 using System.Windows.Forms;
 
-
-
 namespace HospodaUBobra
 {
     public partial class Form1 : Form
     {
-        string connectionString = "User Id=st69607;Password=;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=fei-sql3.upceucebny.cz)(PORT=1521)))(CONNECT_DATA=(SID=BDAS)));";
+        string st = "st69607";
+        string heslo = "Yeet1*";
+        string connectionString;
 
         public Form1()
         {
             InitializeComponent();
-            Log("Zacatek");
-            LoadData();
+            comboBoxTables.SelectedIndexChanged += new EventHandler(comboBoxTables_SelectedIndexChanged);
+            connectionString = $"User Id={st};Password={heslo};Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=fei-sql3.upceucebny.cz)(PORT=1521)))(CONNECT_DATA=(SID=BDAS)));";
+            PopulateTableList();
         }
 
-        private void LoadData()
+        private void PopulateTableList()
         {
             try
             {
-                Log("Attempting to connect to the Oracle database...");
-
                 using (OracleConnection conn = new OracleConnection(connectionString))
                 {
-                    Log("Opening Oracle connection...");
-
                     conn.Open();
 
-                    Log("Successfully connected to the Oracle database.");
+                    string query = $"SELECT table_name FROM all_tables WHERE owner = UPPER('{st}')";
 
-                    using (OracleCommand cmd = new OracleCommand("SELECT * FROM pracovnik", conn))
+                    using (OracleCommand cmd = new OracleCommand(query, conn))
                     {
-                        Log("Executing SQL query: SELECT * FROM pracovnik");
+                        using (OracleDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                comboBoxTables.Items.Add(reader.GetString(0));
+                            }
+                        }
+                    }
 
+                    conn.Close();
+                }
+            }
+            catch (OracleException ex)
+            {
+                Log("Oracle error while fetching table names: " + ex.Message);
+                MessageBox.Show("Oracle error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Log("General error: " + ex.Message);
+                MessageBox.Show("General error: " + ex.Message);
+            }
+        }
+
+        private void comboBoxTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedTable = comboBoxTables.SelectedItem.ToString();
+            LoadTableData(selectedTable);
+        }
+
+        private void LoadTableData(string tableName)
+        {
+            try
+            {
+                using (OracleConnection conn = new OracleConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = $"SELECT * FROM {tableName}";
+
+                    using (OracleCommand cmd = new OracleCommand(query, conn))
+                    {
                         using (OracleDataAdapter adapter = new OracleDataAdapter(cmd))
                         {
                             DataTable dataTable = new DataTable();
                             adapter.Fill(dataTable);
 
-                            Log("Data retrieved successfully.");
-
                             dataGridView1.DataSource = dataTable;
-
-                            Log("DataGridView updated with data.");
                         }
                     }
 
                     conn.Close();
-
-                    Log("Oracle connection closed.");
                 }
             }
             catch (OracleException ex)
@@ -68,7 +99,7 @@ namespace HospodaUBobra
 
         private void Log(string message)
         {
-            MessageBox.Show(message);
+            MessageBox.Show(DateTime.Now + ": " + message);
         }
     }
 }
