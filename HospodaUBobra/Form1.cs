75f031579bdf2fd2b200fa1fb6b97d25090abd97
Raris909 <st69639@upce.cz>
2024-12-07 16:50:05 +0100

    CSV regex changed

--- HospodaUBobra/Form1.cs
+++ HospodaUBobra/Form1.cs
@@ -1575,7 +1575,7 @@ namespace HospodaUBobra
                     {
                         csvContent.Append(table.Columns[i].ColumnName);
                         if (i < table.Columns.Count - 1)
-                            csvContent.Append(",");
+                            csvContent.Append(";");
                     }
                     csvContent.AppendLine();
 
@@ -1585,7 +1585,7 @@ namespace HospodaUBobra
                         {
                             csvContent.Append(row[i].ToString());
                             if (i < table.Columns.Count - 1)
-                                csvContent.Append(",");
+                                csvContent.Append(";");
                         }
                         csvContent.AppendLine();
                     }
